4cc64f852b059428e0c2429b4ebe621dcd8b5da9
iatenine <iatenine@gmail.com>
2021-07-26 15:25:16 -0600

    Added basic smoke tests

--- test.js
+++ test.js
@@ -1,2 +1,13 @@
-const root = document.getElementById("root");
-assert(root !== null, "root is null");
+// That's right, vanilla javascript testing!!
+const checkNull = (id) => {
+  const el = document.getElementById(id);
+  if (el === null) {
+    console.error(`Element with id ${id} not found`);
+    return;
+  }
+  console.log(`${id} is defined`);
+};
+
+referenceIdNames = ["root", "main", "aside", "nav"];
+
+referenceIdNames.map((elem) => checkNull(elem));
