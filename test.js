154545ea2c7a30638a8c80fecc0183865b9e06a9
Jack Linhart <iatenine@gmail.com>
2021-07-26 20:50:55 -0600

    Base skeleton laid out

--- test.js
+++ test.js
@@ -1,4 +1,8 @@
 // That's right, vanilla javascript testing!!
+
+const developerName;
+const 
+
 const checkNull = (id) => {
   const el = document.getElementById(id);
   if (el === null) {
@@ -11,3 +15,7 @@ const checkNull = (id) => {
 referenceIdNames = ["root", "main", "aside", "nav"];
 
 referenceIdNames.map((elem) => checkNull(elem));
+const rootElem = document.getElementById("root");
+const navElem = document.getElementById("nav");
+console.log(navElem.getChildren());
+console.log(rootElem.innerHTML.includes("webpage"));
